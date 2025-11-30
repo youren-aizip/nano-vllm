5583933905451cb49ed7b651989c8e10db4e8793
youren-aizip <youren@aizip.ai>
2025-11-30 14:26:07 -0800

    [note] Gumbel-Max trick in Sampler

--- nanovllm/layers/sampler.py
+++ nanovllm/layers/sampler.py
@@ -3,7 +3,6 @@ from torch import nn
 
 
 class Sampler(nn.Module):
-
     def __init__(self):
         super().__init__()
 
@@ -11,5 +10,7 @@ class Sampler(nn.Module):
     def forward(self, logits: torch.Tensor, temperatures: torch.Tensor):
         logits = logits.float().div_(temperatures.unsqueeze(dim=1))
         probs = torch.softmax(logits, dim=-1)
+        # Gumbel-Max trick
+        # if E_i ~ Exp(1), then the distribution of argmax(p_i / E_i) is the same as the sampling from [p_1, p_2, ...]
         sample_tokens = probs.div_(torch.empty_like(probs).exponential_(1).clamp_min_(1e-10)).argmax(dim=-1)
         return sample_tokens
