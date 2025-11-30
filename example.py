283f4b75ffc15c22cb66638c130e2c91c45d021f
youren-aizip <youren@aizip.ai>
2025-11-29 22:00:47 -0800

    [note] change exmaple for local debug

--- example.py
+++ example.py
@@ -4,9 +4,9 @@ from transformers import AutoTokenizer
 
 
 def main():
-    path = os.path.expanduser("~/huggingface/Qwen3-0.6B/")
+    path = os.path.expanduser("/mnt/localnvme/youren/huggingface/Qwen3-0.6B/")
     tokenizer = AutoTokenizer.from_pretrained(path)
-    llm = LLM(path, enforce_eager=True, tensor_parallel_size=1)
+    llm = LLM(path, enforce_eager=True, tensor_parallel_size=2)
 
     sampling_params = SamplingParams(temperature=0.6, max_tokens=256)
     prompts = [
